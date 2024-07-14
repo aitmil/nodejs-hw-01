39334147a39ff1680ee85746e03a9df1b0aaecdf
Anna Taranchuk <taranchuk4@yahoo.com>
2024-07-14 23:17:50 +0300

    step 2

--- src/scripts/generateContacts.js
+++ src/scripts/generateContacts.js
@@ -4,19 +4,21 @@ import { createFakeContact } from '../utils/createFakeContact.js';
 
 const generateContacts = async (number) => {
   try {
-    // const data = await fs.readFile(PATH_DB, 'utf-8');
-    // let contacts = JSON.parse(data);
-    let contacts = [];
+    const data = await fs.readFile(PATH_DB, 'utf-8');
+    let contacts = JSON.parse(data);
 
     for (let i = 0; i < number; i++) {
       const newContact = createFakeContact();
       contacts.push(newContact);
     }
+
     await fs.writeFile(
       PATH_DB,
       JSON.stringify(contacts, undefined, 2),
       'utf-8',
     );
+
+    console.log(`Successfully generated ${number} contacts.`);
   } catch (error) {
     console.log(error);
   }
