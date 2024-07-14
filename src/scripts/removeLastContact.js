8ebef0cb720af6f88b2f37a7dbccdcbc029adbfa
Anna Taranchuk <taranchuk4@yahoo.com>
2024-07-15 00:20:34 +0300

    step 7

--- src/scripts/removeLastContact.js
+++ src/scripts/removeLastContact.js
@@ -1,5 +1,28 @@
+import * as fs from 'node:fs/promises';
 import { PATH_DB } from '../constants/contacts.js';
 
-export const removeLastContact = async () => {};
+export const removeLastContact = async () => {
+  try {
+    const data = await fs.readFile(PATH_DB, 'utf-8');
+    const contacts = JSON.parse(data);
+
+    if (contacts.length === 0) {
+      console.log('There are no contacts in collection.');
+      return;
+    }
+
+    const removedContact = contacts.pop();
+
+    await fs.writeFile(
+      PATH_DB,
+      JSON.stringify(contacts, undefined, 2),
+      'utf-8',
+    );
+
+    console.log(`Successfully removed last (${removedContact.name}) contact`);
+  } catch (error) {
+    console.log(error);
+  }
+};
 
 removeLastContact();
