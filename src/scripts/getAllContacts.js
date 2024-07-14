2ee1b40ff1036b103090443e33563be48bc56b9f
Anna Taranchuk <taranchuk4@yahoo.com>
2024-07-14 23:34:14 +0300

    step 4

--- src/scripts/getAllContacts.js
+++ src/scripts/getAllContacts.js
@@ -1,5 +1,13 @@
+import * as fs from 'node:fs/promises';
 import { PATH_DB } from '../constants/contacts.js';
 
-export const getAllContacts = async () => {};
+export const getAllContacts = async () => {
+  try {
+    const data = await fs.readFile(PATH_DB, 'utf-8');
+    return JSON.parse(data);
+  } catch (error) {
+    console.log(error);
+  }
+};
 
 console.log(await getAllContacts());
