85b429ae990127406cc903751f80f7525212029b
Anna Taranchuk <taranchuk4@yahoo.com>
2024-07-15 00:20:22 +0300

    step 6

--- src/scripts/removeAllContacts.js
+++ src/scripts/removeAllContacts.js
@@ -1,5 +1,20 @@
+import * as fs from 'node:fs/promises';
 import { PATH_DB } from '../constants/contacts.js';
 
-export const removeAllContacts = async () => {};
+export const removeAllContacts = async () => {
+  try {
+    const contacts = [];
+
+    await fs.writeFile(
+      PATH_DB,
+      JSON.stringify(contacts, undefined, 2),
+      'utf-8',
+    );
+
+    console.log('Successfully removed all contacts');
+  } catch (error) {
+    console.log(error);
+  }
+};
 
 removeAllContacts();
